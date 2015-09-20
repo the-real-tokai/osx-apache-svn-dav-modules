f9ec4b7fb9fa75e60dd8e242daf3702cc222474f
Christian Rosentreter <the-real-tokai@users.noreply.github.com>
2015-09-20 18:18:38 +0200

    Tweaked to build without "/usr/include" (which is the default on most OS X installations.)

--- BuildSubversionDAVModules.sh
+++ BuildSubversionDAVModules.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 
-#  BuildSubversionDAVModules.sh
-#  by tokai (http://tokai.binaryriot.org/), 15-Mar-2015 
+#  BuildSubversionDAVModules.sh v2
+#  by tokai (http://tokai.binaryriot.org/), 20-Sep-2015 
 #
 #  Synopsis: This script builds matching 'mod_auth_svn.so' and 'mod_dav_svn.so' for Mac OS X 10.10 (Yosemite)
-#            and Xcode 6 for use with Apache's httpd. For some reason Apple doesn't manage it anymore to bundle
+#            and Xcode 7 for use with Apache's httpd. For some reason Apple doesn't manage it anymore to bundle
 #            the modules somewhere in their releases. Still both modules are required to set up Subversion
 #            repository access via http and/or https.
 #
@@ -16,19 +16,19 @@
 
 set -ue
 
+
+SDKPATH=$(xcrun --show-sdk-path)
+
+
 # Check if "/usr/include" exists…
 #
 echo 'Checking for "/usr/include"…'
 if [ ! -d "/usr/include" ]; then
-	# Grab "version.revision" and skip ".subrevision"…
-	osx_version=`sw_vers -productVersion | sed -n 's/\(^[0-9]\{1,2\}\)\.\([0-9]\{1,2\}\).*$/\1.\2/p'`
-		
-	printf 'Error: "/usr/include" is required to build Apache Subversion. Make sure that Xcode is installed. '
-	printf 'In case the directory is missing anyway then making it a softlink to "'
-	printf '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX'$osx_version'.sdk/usr/include'
-	printf '" will work too.\n'
-	exit 1
+	printf 'Warning: "/usr/include" is required to build Apache Subversion properly. Make sure that Xcode is installed. '
+	printf 'In some cases it can be useful to additionally install the Xcode Command Line Tools (CLT) with "xcode-select --install" or manually '
+	printf 'setting up "/usr/include" as a softlink pointing to "%s".\n' $SDKPATH
 fi
+
 
 # Grab the exact version of svnadmin…
 # (assumes it is in the format of "version.revision.subrevision")
@@ -41,70 +41,69 @@ if [ -z "svnadmin_version" ]; then
 	exit 1
 fi
 
+
 # Fetch a matching version from the Apache servers and unpack it…
 #
 echo 'Downloading "subversion-'$svnadmin_version'.tar.bz2"…'
 curl 'http://archive.apache.org/dist/subversion/subversion-'$svnadmin_version'.tar.bz2' > 'subversion-'$svnadmin_version'.tar.bz2'
 echo 'Unpacking source code…'
-bunzip2 'subversion-'$svnadmin_version'.tar.bz2'
-tar -xf 'subversion-'$svnadmin_version'.tar'
+tar -xvyf 'subversion-'$svnadmin_version'.tar.bz2'
+
 
 ( if cd "subversion-$svnadmin_version" ; then
-	
+
 	# Build the two Apache modules…
 	#
-	echo 'Configuring Subversion build…'
-	./configure         >./build.log   2>./build_strerr.log
-	echo 'Making "mod_dav_svn"…'
-	make mod_dav_svn    >>./build.log  2>>./build_strerr.log
-	echo 'Making "mod_authz_svn"…'
-	make mod_authz_svn  >>./build.log  2>>./build_strerr.log
-	
-	# Fix up the two Apache modules…
-	#
-	# Info: The resulting modules contain references to '/usr/local/lib' and expect various libraries (*.dylib)
-	#       of Subversion to be located there. Since we have those libraries already inside the Xcode.app bundle,
-	#       we simply update the internal links to point to those versions (else it would fail when httpd loads
-	#       the modules.) Alternatively a second set of the libraries could be installed to '/usr/local/lib'
-	#       via 'make install', but that is a pointless waste of disk space, IMHO.
-	#
-	
-	echo 'Fixing dynamic library paths in "mod_dav_svn"…'
-	liblist=`otool -L subversion/mod_dav_svn/.libs/mod_dav_svn.so | sed -n 's/^'$'\t''\/usr\/local\/lib\/libsvn_\(.*\).dylib .*$/\1/p'`
-	for lib in $liblist; do
-		echo 'Fixing path of "libsvn_'$lib'" in mod_dav_svn.so…'
-		install_name_tool -change '/usr/local/lib/libsvn_'"$lib"'.dylib' '/Applications/Xcode.app/Contents/Developer/usr/lib/libsvn_'"$lib"'.dylib' 'subversion/mod_dav_svn/.libs/mod_dav_svn.so'
-	done
-	# Output used library list for verification…
-	#otool -L 'subversion/mod_dav_svn/.libs/mod_dav_svn.so'
-	
-	echo 'Fixing dynamic library paths in "mod_authz_svn"…'
-	liblist=`otool -L subversion/mod_authz_svn/.libs/mod_authz_svn.so | sed -n 's/^'$'\t''\/usr\/local\/lib\/libsvn_\(.*\).dylib .*$/\1/p'`
-	for lib in $liblist; do
-		echo 'Fixing path of "libsvn_'$lib'" in mod_authz_svn.so…'
-		install_name_tool -change '/usr/local/lib/libsvn_'"$lib"'.dylib' '/Applications/Xcode.app/Contents/Developer/usr/lib/libsvn_'"$lib"'.dylib' 'subversion/mod_authz_svn/.libs/mod_authz_svn.so'
-	done
-	# Output used library list for verification…
-	#otool -L 'subversion/mod_authz_svn/.libs/mod_authz_svn.so'
-	
-	#
-	# TODO: Maybe it would be smarter and go quicker to just build the two libs with the proper paths? :-P
+	# Note: The build system is a broken disaster (perhaps related to invalid paths provided by OS X's
+	#       "apr-1-config" and "apu-1-config"?), so let's try our best by tweaking the include paths, etc.,
+	#       manually in case "/usr/include" is missing (which is the default situation on most OS X
+	#       installations.)
 	#	
+	IFLAGS="-I$SDKPATH/usr/include -I$SDKPATH/usr/include/apr-1 -I$SDKPATH/usr/include/apache2"
+	
+	echo 'Configuring Subversion build…'
+	./configure \
+		--prefix=/Applications/Xcode.app/Contents/Developer/usr \
+		--disable-debug \
+		--with-zlib=/usr \
+		--disable-mod-activation \
+		--with-apache-libexecdir=$(/usr/sbin/apxs -q libexecdir) \
+		--without-berkeley-db \
+		--disable-nls \
+		--without-serf \
+		--with-apr=/usr \
+		--with-apr-util=/usr \
+		--with-apxs=no \
+		CFLAGS="$IFLAGS" CXXFLAGS="$IFLAGS" CPPFLAGS="$IFLAGS" 
+	
+	echo 'Making modules…'
+	make mod_dav_svn mod_authz_svn
+
 else
 	echo 'Error: Could not locate Apache Subversion source code.'
 	exit 1
 fi )
 
+
+#  Dump used libs…
+#
+otool -L "subversion-${svnadmin_version}/subversion/mod_dav_svn/.libs/mod_dav_svn.so"
+otool -L "subversion-${svnadmin_version}/subversion/mod_authz_svn/.libs/mod_authz_svn.so"
+
+
 #  Clean up…
 #
 echo 'Copying modules to current directory…'
-mv "subversion-$svnadmin_version/subversion/mod_dav_svn/.libs/mod_dav_svn.so" './mod_dav_svn.so'
-mv "subversion-$svnadmin_version/subversion/mod_authz_svn/.libs/mod_authz_svn.so" './mod_authz_svn.so'
+mv "subversion-${svnadmin_version}/subversion/mod_dav_svn/.libs/mod_dav_svn.so" './mod_dav_svn.so'
+mv "subversion-${svnadmin_version}/subversion/mod_authz_svn/.libs/mod_authz_svn.so" './mod_authz_svn.so'
+
 
 echo 'Deleting temporary files…'
-rm -rf "subversion-$svnadmin_version"
-rm -f "subversion-$svnadmin_version.tar"
+rm -rf "subversion-${svnadmin_version}"
+rm -f "subversion-${svnadmin_version}.tar.bz2"
+
 
 printf '\n\nAll done!\n'
+
 
 exit 0
